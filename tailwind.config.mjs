31c53bd95653426adee2a73b029c0ed4b60f8976
David Osorio <daalosx@gmail.com>
2024-09-24 14:45:50 -0500

    agregando navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,13 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'black': '#0F0E0D',
+			'white': '#FAF7F0',
+			'dark-blue': '#98A8F8',
+			'blue': '#BCCEF8',
+			'light-blue': '#CDFCF6',
+		},
 	},
 	plugins: [],
 }
