136d1281e9463297568384c692e093c00f37d657
David Osorio <daalosx@gmail.com>
2024-09-25 17:08:03 -0500

    terminando la navbar

--- src/components/react/Menu.tsx
+++ src/components/react/Menu.tsx
@@ -1,28 +1,33 @@
 import { useState } from "react";
 
-export default function Menu() {
+interface Props {
+  PrinSelected: boolean;
+  TrabSelected: boolean;
+  ContSelected: boolean;
+}
+
+export default function Menu({PrinSelected, TrabSelected, ContSelected}: Props) {
   const [showMenu, setShowMenu] = useState(false);
 
   const handleClick = () => {
     setShowMenu(!showMenu);
-    console.log(showMenu);
   };
 
   return (
     <span className="flex flex-col w-fit justify-between items-end min-h-[200px] h-auto absolute top-3 right-3 min-[685px]:hidden">
       <button className="cursor-hand z-50" onClick={handleClick}>
-        <img src="/menu.svg" alt="menu" className="size-12 rounded-full bg-black/10 p-2 block active:bg-blue" />
+        <img src="/menu.svg" alt="menu" className={`size-12 rounded-full ${showMenu ? "bg-light-blue" : "bg-black/10"} p-2 block active:bg-blue`} />
       </button>
       {showMenu && (
         <ul className="flex flex-col items-center rounded-b-xl bg-black/10 text-black/80 text-lg font-bold z-10">
-          <li className="py-2 px-3 hover:bg-light-blue">
-            <a href="/" className="block">🏠 principal</a>
+          <li className={`py-2 px-3 w-full ${PrinSelected ? "bg-light-blue" : ""} ${PrinSelected ? "hover:bg-blue" : "hover:bg-light-blue"}`}>
+            <a href="/" className="block">🏠 Principal</a>
           </li>
-          <li className="py-2 px-3 hover:bg-light-blue">
+          <li className={`py-2 px-3 w-full ${TrabSelected ? "bg-light-blue" : ""} ${TrabSelected ? "hover:bg-blue" : "hover:bg-light-blue"}`}>
             <a href="/trabajos" className="block">🧾 Trabajos</a>
           </li>
-          <li className="py-2 px-3 rounded-b-xl hover:bg-light-blue">
-            <a href="/contactanos" className="block">📲 contactanos</a>
+          <li className={`py-2 px-3 w-full rounded-b-xl ${ContSelected ? "bg-light-blue" : ""} ${ContSelected ? "hover:bg-blue" : "hover:bg-light-blue"}`}>
+            <a href="/contactanos" className="block">📲 Contactanos</a>
           </li>
         </ul>
       )}
