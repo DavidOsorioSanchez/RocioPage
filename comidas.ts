57790af7fe45571afe608c9ffd42f5e234743225
David Osorio <daalosx@gmail.com>
2024-10-02 18:09:48 -0500

    agregando escalabilidad

--- comidas.ts
+++ comidas.ts
@@ -1,37 +1,37 @@
 export const comidas= [
     {
         id : 0,
-        bgImage: "bg-curry",
+        bgImage: "curry.jpeg",
         title: "Curry",
         price: "€ 10"
     },
     {
         id: 1,
-        bgImage: "bg-pasta",
+        bgImage: "pasta.jpeg",
         title: "Boloñesa",
         price: "€ 10"
     },
     {
         id: 2,
-        bgImage: "bg-burritos",
+        bgImage: "burritos.jpeg",
         title: "Burritos",
         price: "€ 10"
     },
     {
         id: 3,
-        bgImage: "bg-sopon",
+        bgImage: "sopon.jpeg",
         title: "Sopon Marinero",
         price: "€ 10"
     },
     {
         id: 4,
-        bgImage: "bg-apple",
+        bgImage: "apple.jpeg",
         title: "Apple Pie",
         price: "€ 20"
     },
     {
         id: 5,
-        bgImage: "bg-yogurt",
+        bgImage: "yogurt.jpeg",
         title: "Yogurt",
         price: "€ 20"
     }
--- src/components/react/ProductsCards.tsx
+++ src/components/react/ProductsCards.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {comidas} from '../../../comidas.ts';
 
 
@@ -6,8 +5,9 @@ export default function ProductsCards() {
     return (
         <div className="grid grid-cols-1 gap-8 w-full tablet:grid-cols-2 laptop:grid-cols-3 ">
             {comidas.map((data: any) =>(
-                <article key={data.id} className={`${data.bgImage} relative bg-cover aspect-4/3 bg-center bg-no-repeat rounded-lg w-full h-72 shadow-xl shadow-black/60 border-double border-2 border-black/30 transition-all duration-150 animate-expandir hover:scale-[1.02] active:brightness-75`}>
-                    <footer className="absolute bottom-0 w-full h-fit flex items-center justify-center gap-2 bg-black/80 p-2 text-white rounded-b-lg">
+                <article className="relative rounded-lg w-full h-72 shadow-xl shadow-black/60 border-double border-2 border-black/30 transition-all duration-150 animate-expandir hover:scale-[1.02] active:brightness-75">
+                    <img src={`/recetas/${data.bgImage}`} alt={`Una foto de${data.title}`} className="absolute top-0 left-0 w-full h-full aspect-4/3 object-cover rounded-lg" />
+                    <footer className="absolute bottom-0 w-full h-fit flex items-center justify-center gap-2 bg-black/80 p-2 text-white rounded-b-lg z-20">
                         <p className="text-xl font-semibold">
                             {data.title}
                         </p>
