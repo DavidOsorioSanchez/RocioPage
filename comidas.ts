8090a94b0e70ca7843076448a3b773896df54dbd
David Osorio <daalosx@gmail.com>
2024-10-02 21:12:00 -0500

    agregando todo el menu

--- comidas.ts
+++ comidas.ts
@@ -1,38 +1,98 @@
 export const comidas= [
     {
-        id : 0,
         bgImage: "curry.jpeg",
         title: "Curry",
-        price: "€ 10"
+        price: "€ 10",
+        modalActivado: false,
+        descripcion: "El curry es receta de origenes Indu 🇮🇳. Conocido por su aroma y sabor ademas de caracterizado por su picante.",
+        carousel: false,
+        imagenes: ["curry.jpeg"],
+        picante: true,
+        categoria: ['Almuerzo', 'Cena'],
+        URL: "https://wa.me/573104745978?text=¡Hola!%20Quiero%20Ordenar%20un%20curry🍛."
     },
     {
-        id: 1,
         bgImage: "pasta.jpeg",
-        title: "Boloñesa",
-        price: "€ 10"
+        title: "pasta boloñesa",
+        price: "€ 10",
+        modalActivado: false,
+        descripcion: "La pasta boloñesa es una receta de origenes Italianos 🇮🇹. Con un sabor muy caracteristico y una suave textura.",
+        carousel: false,
+        imagenes: ["pasta.jpeg"],
+        picante: false,
+        categoria: ['Almuerzo', 'Cena'],
+        URL: "https://wa.me/573104745978?text=¡Hola!%20Quiero%20Ordenar%20una%20Pasta%20boloñesa🍝."
     },
     {
-        id: 2,
         bgImage: "burritos.jpeg",
         title: "Burritos",
-        price: "€ 10"
+        price: "€ 10",
+        modalActivado: false,
+        descripcion: "Los burritos son una receta de origenes Mexicanos 🇲🇽. Conocido internacionalmente por su sabor y picante.",
+        carousel: false,
+        imagenes: ["burritos.jpeg"],
+        picante: true,
+        categoria: ['Almuerzo', 'Cena'],
+        URL: "https://wa.me/573104745978?text=¡Hola!%20Quiero%20Ordenar%20un%20burrito🌯."
     },
     {
-        id: 3,
         bgImage: "sopon.jpeg",
         title: "Sopon Marinero",
-        price: "€ 10"
+        price: "€ 10",
+        modalActivado: false,
+        descripcion: "El sopon marinero es una receta de origenes Cubanos 🇨🇺. Una sopa de maricos inrecistibles.",
+        carousel: true,
+        imagenes: ["sopon.jpeg", "sopon2.jpeg"],
+        picante: false,
+        categoria: ['Almuerzo', 'Cena'],
+        URL: "https://wa.me/573104745978?text=¡Hola!%20Quiero%20Ordenar%20un%20sopon%20marinero🍲."
     },
     {
-        id: 4,
         bgImage: "apple.jpeg",
         title: "Apple Pie",
-        price: "€ 20"
+        price: "€ 20",
+        modalActivado: false,
+        descripcion: "La pie de manzana es una receta de origenes Estadounidense 🇺🇸. Un postre de manzana y canela especialmente delicioso.",
+        carousel: true,
+        imagenes: ["apple.jpeg" , "apple2.jpeg"],
+        picante: false,
+        categoria: ['Postre', 'Desayuno'],
+        URL: "https://wa.me/573104745978?text=¡Hola!%20Quiero%20Ordenar%20una%20pie%20de%20manzana🥧."
     },
     {
-        id: 5,
         bgImage: "yogurt.jpeg",
         title: "Yogurt",
-        price: "€ 20"
+        price: "€ 20",
+        modalActivado: false,
+        descripcion: "El yogurt es una receta de origenes Turquos 🇹🇷. Adicionado de frutas, es un postre delicioso y rico en nutrientes.",
+        carousel: false,
+        imagenes: ["yogurt.jpeg"],
+        picante: false,
+        categoria: ['Postre', 'Desayuno', 'Bebida'],
+        URL: "https://wa.me/573104745978?text=¡Hola!%20Quiero%20Ordenar%20un%20litro%20de%20yogurt🥛."
+    },
+    {
+        bgImage: "ArrozLeche.jpeg",
+        title: "Arroz con leche",
+        price: "€ 20",
+        modalActivado: false,
+        descripcion: "El arroz leche es una receta de origenes Colombiano 🇨🇴. Un postre tipico de la región, con una variedad de sabores unicos.",
+        carousel: false,
+        imagenes: ["ArrozLeche.jpeg"],
+        picante: false,
+        categoria: ['Postre', 'Desayuno'],
+        URL: "https://wa.me/573104745978?text=¡Hola!%20Quiero%20Ordenar%20arroz%20con%20leche🍚."
+    },
+    {
+        bgImage: "Kefir.jpeg",
+        title: "Kefir",
+        price: "€ 20",
+        modalActivado: false,
+        descripcion: "El kefir es una receta de origenes Rusos 🇷🇺. Una bebida deliciosa y rica en probioticos.",
+        carousel: false,
+        imagenes: ["Kefir.jpeg"],
+        picante: false,
+        categoria: ['Postre', 'Bebida'],
+        URL: "https://wa.me/573104745978?text=¡Hola!%20Quiero%20Ordenar%20un%20litro%20de%20kefir🥛."
     }
 ]
--- src/components/react/ProductsCards.tsx
+++ src/components/react/ProductsCards.tsx
@@ -1,21 +1,40 @@
-import {comidas} from '../../../comidas.ts';
+import { comidas } from '../../../comidas.ts';
 
+interface ParametroComidas {
+    bgImage: string;
+    title: string;
+    price: string;
+    modalActivado?: boolean;
+    descripcion?: string;
+    carousel?: boolean;
+    imagenes?: string[];
+    picante?: boolean;
+    categoria?: string[];
+    URL?: string;
+}
 
 export default function ProductsCards() {
+        
     return (
         <div className="grid grid-cols-1 gap-8 w-full tablet:grid-cols-2 laptop:grid-cols-3 ">
-            {comidas.map((data: any) =>(
-                <article className="relative rounded-lg w-full h-72 shadow-xl shadow-black/60 border-double border-2 border-black/30 transition-all duration-150 animate-expandir hover:scale-[1.02] active:brightness-75">
+            {comidas.map((data: ParametroComidas) =>(
+                <button
+                    key={data.title}
+                    onClick={() =>(
+                        console.log(data)
+                    )}
+                    className="relative rounded-lg w-full h-72 shadow-xl shadow-black/60 border-double border-2 border-black/30 transition-all duration-150 animate-expandir hover:scale-[1.02] active:brightness-75"
+                >
                     <img src={`/recetas/${data.bgImage}`} alt={`Una foto de${data.title}`} className="absolute top-0 left-0 w-full h-full aspect-4/3 object-cover rounded-lg" />
                     <footer className="absolute bottom-0 w-full h-fit flex items-center justify-center gap-2 bg-black/80 p-2 text-white rounded-b-lg z-20">
-                        <p className="text-xl font-semibold">
+                        <p className="text-xl font-semibold capitalize">
                             {data.title}
                         </p>
                         <p className="text-sm">
                             {data.price}
                         </p>
                     </footer>
-                </article>
+                </button>
             ))}
         </div>
     )
