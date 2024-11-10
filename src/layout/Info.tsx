833781422eff92224df1321819f2c1b02e6e8421
David Osorio <daalosx@gmail.com>
2024-11-09 20:56:06 -0500

    logo de gmail

--- src/layout/Info.tsx
+++ src/layout/Info.tsx
@@ -35,7 +35,7 @@ export default function Info() {
                     </CopyButton>
                     <CopyButton textoCopiar="a.osorio@misena.edu.co" width="w-64" animation="animate-aparecerLadoDerecho">
                         <span className="w-fit text-lg font-bold flex items-center text-black/80">
-                            <img src="/gmail.svg" alt="gmail icono" className="size-10 " />
+                        <svg xmlns="http://www.w3.org/2000/svg" className='size-10' aria-label="Gmail" role="img" viewBox="0 0 512 512" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><rect width="512" height="512" rx="15%" fill="#e3dfda"></rect><path d="M158 391v-142l-82-63V361q0 30 30 30" fill="#4285f4"></path><path d="M 154 248l102 77l102-77v-98l-102 77l-102-77" fill="#ea4335"></path><path d="M354 391v-142l82-63V361q0 30-30 30" fill="#34a853"></path><path d="M76 188l82 63v-98l-30-23c-27-21-52 0-52 26" fill="#c5221f"></path><path d="M436 188l-82 63v-98l30-23c27-21 52 0 52 26" fill="#fbbc04"></path></g></svg>
                             a.osorio@misena.edu.co
                         </span>
                     </CopyButton>
